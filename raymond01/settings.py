153e0a1a762287f3b50ae1fa4e9be91aab947382
Raymond <yue_raymond@yahoo.com.hk>
2025-05-29 11:50:12 +0800

    Step 5 : install models

--- raymond01/settings.py
+++ raymond01/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'raymond01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcredb',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST' : 'localhost'
     }
 }
 
@@ -139,3 +142,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
