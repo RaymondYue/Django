4b22059ef5f27b8a1864254766db9a236ebe81fa
Raymond <yue_raymond@yahoo.com.hk>
2025-06-02 16:16:04 +0800

    step 9: listing page

--- raymond01/settings.py
+++ raymond01/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SITE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware,WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
